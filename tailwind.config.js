23e5efdecb88d87c0e9d3206184fa90a73e80018
dev derah <chidera261@gmail.com>
2023-10-13 06:23:07 +0100

    Setup

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#CCFB00",
+        secondary: {
+          DEFAULT: "#FF7A00",
+          50: "#FFF2E6",
+          800: "#8C4300",
+        },
+        success: {
+          DEFAULT: "#2CD111",
+          50: "#EAFAE7",
+          100: "#FF7A00",
+          800: "#187309",
+        },
+        error: {
+          DEFAULT: "#BA1A1A",
+          50: "#F8E8E8",
+        },
+        neutral: {
+          DEFAULT: " #F0F0F0",
+          3: "#F5F5F5",
+          4: "#F0F0F0",
+          5: "#D9D9D9",
+          6: "#BFBFBF",
+          7: "#8C8C8C",
+          8: "#595959",
+        },
+        gray:"#A8A8A8",
+
+      },
+    },
+  },
+  plugins: [],
+};
